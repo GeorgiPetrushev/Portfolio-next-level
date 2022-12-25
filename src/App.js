9d2bad20b504bc57652c128b1a6aed6c38106a7e
Georgi <figoto660@gmail.com>
2022-12-24 23:57:08 -0600

    add nav bar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import StyledApp from "./StyleApp";
-import Header from './components/header';
+import Header from "./components/header/Header";
+import Home from './components/home/Home';
+import About from './components/about/About';
+import Skills from './components/skills/Skills';
+import Qualification from './components/qualification/Qualification';
+import Work from './components/work/Work';
+import Contact from './components/contact/Contact';
+import Footer from './components/footer/Footer';
+import ScrollUp from './components/scrollup/ScrollUp';
+import './App.css'
 
 function App() {
   return (
     <StyledApp>
       <Header />
 
-      {/* <main className="main">
+      <main className="main">
         <Home />
         <About />
         <Skills />
@@ -16,7 +25,7 @@ function App() {
       </main>
 
       <Footer />
-      <ScrollUp /> */}
+      <ScrollUp />
     </StyledApp>
   );
 }
