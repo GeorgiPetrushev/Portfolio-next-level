28bb275c8c1805e998b0f533464ba1608ad27b27
Georgi <figoto660@gmail.com>
2022-12-29 23:49:20 -0500

    .

--- src/components/home/Social.jsx
+++ src/components/home/Social.jsx
@@ -1,15 +1,25 @@
 import React from "react";
-import {CiLinkedin} from 'react-icons/ci';
-import {FiGithub} from 'react-icons/fi';
+import { CiLinkedin } from "react-icons/ci";
+import { FiGithub } from "react-icons/fi";
 
 const Social = () => {
   return (
     <div className="home__social">
-      <a href="https://www.linkedin.com/in/georgi-petrushev-9b867921a/" className="home__social-icon">
+      <a
+        target="_blank"
+        rel="noopener noreferrer"
+        href="https://www.linkedin.com/in/georgi-petrushev-9b867921a/"
+        className="home__social-icon"
+      >
         <CiLinkedin />
       </a>
 
-      <a href="https://github.com/GeorgiPetrushev" className="home__social-icon">
+      <a
+        target="_blank"
+        rel="noopener noreferrer"
+        href="https://github.com/GeorgiPetrushev"
+        className="home__social-icon"
+      >
         <FiGithub />
       </a>
     </div>
