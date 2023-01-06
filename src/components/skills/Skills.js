833e2b912e5506b05472eb79a3fa090551390856
Georgi <figoto660@gmail.com>
2023-01-06 22:13:24 +0200

    skills

--- src/components/skills/Skills.js
+++ src/components/skills/Skills.js
@@ -1,9 +1,18 @@
-import React from 'react'
+import Frontend from "./Frontend";
+import Backend from "./Backend";
 
 const Skills = () => {
   return (
-    <div>Skills</div>
-  )
-}
+    <section className="skills section" id="skills">
+      <h2 className="section__title">Skills</h2>
+      <span className="section__subtitle">My technical level</span>
 
-export default Skills
+      <div className="skills__container container grid">
+        <Frontend />
+        <Backend />
+      </div>
+    </section>
+  );
+};
+
+export default Skills;
