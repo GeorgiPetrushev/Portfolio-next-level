c882885a52f093aa952f91fbbacad35e9a4e2b68
Georgi <figoto660@gmail.com>
2023-01-06 22:18:00 +0200

    style

--- src/components/skills/Skills.js
+++ src/components/skills/Skills.js
@@ -1,17 +1,20 @@
 import Frontend from "./Frontend";
 import Backend from "./Backend";
+import StyledSkills from "./StyleSkills";
 
 const Skills = () => {
   return (
-    <section className="skills section" id="skills">
-      <h2 className="section__title">Skills</h2>
-      <span className="section__subtitle">My technical level</span>
+    <StyledSkills>
+      <section className="skills section" id="skills">
+        <h2 className="section__title">Skills</h2>
+        <span className="section__subtitle">My technical level</span>
 
-      <div className="skills__container container grid">
-        <Frontend />
-        <Backend />
-      </div>
-    </section>
+        <div className="skills__container container grid">
+          <Frontend />
+          <Backend />
+        </div>
+      </section>
+    </StyledSkills>
   );
 };
 
