be6cfaa95ea229639846a1086e6c0eee745136c9
Georgi <figoto660@gmail.com>
2023-02-25 22:06:06 -0500

    scroll up

--- src/components/scrollup/ScrollUp.js
+++ src/components/scrollup/ScrollUp.js
@@ -1,9 +1,21 @@
-import React from 'react'
+import React from "react";
+import StyleScrollUp from "./StyleScrollUp";
+import {ImArrowUp} from 'react-icons/im'
 
 const ScrollUp = () => {
+  window.addEventListener("scroll", function () {
+    const scrollUp = document.querySelector(".scrollup");
+    // when the scroll is higher than 560 viewport height, add the show-scroll class to a tag with the scroll-top class
+    if (this.scrollY >= 560) scrollUp.classList.add("show-scroll");
+    else scrollUp.classList.remove("show-scroll");
+  });
   return (
-    <div>ScrollUp</div>
-  )
-}
+    <StyleScrollUp>
+      <a href="#" className="scrollup">
+        <ImArrowUp className="uil uil-arrow-up scrollup__icon"></ImArrowUp>
+      </a>
+    </StyleScrollUp>
+  );
+};
 
-export default ScrollUp
+export default ScrollUp;
