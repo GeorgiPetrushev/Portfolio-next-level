000ba83814d476edca0b456ad57e581a233365ad
Georgi <figoto660@gmail.com>
2023-02-25 22:40:59 -0500

    work

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import StyledApp from "./StyleApp";
 import Header from "./components/header/Header";
-import Home from './components/home/Home';
-import About from './components/about/About';
-import Skills from './components/skills/Skills';
-import Qualification from './components/qualification/Qualification';
-import Work from './components/work/Work';
-import Contact from './components/contact/Contact';
-import Footer from './components/footer/Footer';
-import ScrollUp from './components/scrollup/ScrollUp';
-import './App.css'
+import Home from "./components/home/Home";
+import About from "./components/about/About";
+import Skills from "./components/skills/Skills";
+import Qualification from "./components/qualification/Qualification";
+import Work from "./components/work/Work";
+import Contact from "./components/contact/Contact";
+import Footer from "./components/footer/Footer";
+import ScrollUp from "./components/scrollup/ScrollUp";
+import "./App.css";
 
 function App() {
   return (
