f4837dd00e5feef2a228fe22884f895dd8083f4c
Georgi <figoto660@gmail.com>
2023-03-01 12:34:42 -0600

    footer style

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,66 +1,68 @@
 import React from "react";
-import "./footer.css";
+import StyledFooter from "./StyleFooter";
 
 const Footer = () => {
   return (
-    <footer className="footer">
-      <div className="footer__container container">
-        <h1 className="footer__title">Georgi</h1>
+    <StyledFooter>
+      <footer className="footer">
+        <div className="footer__container container">
+          <h1 className="footer__title">Georgi</h1>
 
-        <ul className="footer__list">
-          <li>
-            <a href="#about" className="footer__link">
-              About
+          <ul className="footer__list">
+            <li>
+              <a href="#about" className="footer__link">
+                About
+              </a>
+            </li>
+
+            <li>
+              <a href="#portfolio" className="footer__link">
+                Projects
+              </a>
+            </li>
+
+            <li>
+              <a href="#skills" className="footer__link">
+                Skills
+              </a>
+            </li>
+          </ul>
+
+          <div className="footer__social">
+            <a
+              href="https://www.facebook.com/"
+              className="footer__social-link"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <i className="bx bxl-facebook"></i>
             </a>
-          </li>
 
-          <li>
-            <a href="#portfolio" className="footer__link">
-              Projects
+            <a
+              href="https://www.instagram.com/"
+              className="footer__social-link"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <i className="bx bxl-instagram"></i>
             </a>
-          </li>
 
-          <li>
-            <a href="#skills" className="footer__link">
-              Skills
+            <a
+              href="https://twitter.com/"
+              className="footer__social-link"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <i className="bx bxl-twitter"></i>
             </a>
-          </li>
-        </ul>
+          </div>
 
-        <div className="footer__social">
-          <a
-            href="https://www.facebook.com/"
-            className="footer__social-link"
-            target="_blank"
-            rel="noreferrer"
-          >
-            <i className="bx bxl-facebook"></i>
-          </a>
-
-          <a
-            href="https://www.instagram.com/"
-            className="footer__social-link"
-            target="_blank"
-            rel="noreferrer"
-          >
-            <i className="bx bxl-instagram"></i>
-          </a>
-
-          <a
-            href="https://twitter.com/"
-            className="footer__social-link"
-            target="_blank"
-            rel="noreferrer"
-          >
-            <i className="bx bxl-twitter"></i>
-          </a>
+          <span className="footer__copy">
+            &#169; Georgi Petrushev 2022. All rigths reserved
+          </span>
         </div>
-
-        <span className="footer__copy">
-          &#169; Georgi Petrushev. All rigths reserved
-        </span>
-      </div>
-    </footer>
+      </footer>
+    </StyledFooter>
   );
 };
 
